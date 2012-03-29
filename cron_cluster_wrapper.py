903c93e4485b647b2c669bd851868134d6f8aafa
Erik Redding <erik.redding@txstate.edu>
2012-03-29 14:45:31 -0500

    modify cron_cluster_wrapper.py; seems to be complete.  Cleaned up is_service_running code to fix logic bug, and tightened up the call to main.

--- cron_cluster_wrapper.py
+++ cron_cluster_wrapper.py
@@ -1,84 +1,84 @@
 #!/usr/bin/env python
 # cron_cluster_wrapper.py
+# By Erik Redding <erik.redding@txstate.edu>
 """
 This is a wrapper that is cluster-aware for cronjobs.  Two-node clusters.
 
 it runs clustat and based on the output, finds cluster members and what 
 services are running on that node.  the argument --service specifies the
-service that is important to this cronjob.  If the member, as provided
-by --node (this should be set to $HOSTNAME or something similar), 
-is running the service, then the wrapper will execute the --script 
+service that is important to this cronjob.  If this member is running the
+service, then the wrapper will return 0, otherwise 1.  
 
+Options:
+--service=<svcname>     CASE SENSITIVE!
 
 Example:
 
 ##### in crontab:
-0 2 0 0 0 /usr/local/sbin/cron_cluster_wrapper.py --service mysql --script=/usr/local/sbin/myscript.sh 
+0 2 0 0 0 /usr/local/sbin/cron_cluster_wrapper.py --service mysql && <your cron script>
 
 
 """
 # 
 
 import os
+import sys
 import subprocess
 import re
 import optparse
 
-
 #cmd_clustat='/usr/sbin/clustat'
-cmd_clustat='./clustat'
+cmd_clustat='./clustat_nonmaster'
 
 def grab_clustat_output( ):
-	""" 
-	this runs clustat and returns the output in a list.  from there, deal with it.
-	"""
-	p = subprocess.Popen( '/usr/sbin/clustat',stdout=subprocess.PIPE ).stdout
-	return p.readlines( )
+    """ 
+    this runs clustat and returns the output in a list.  from there, deal with it.
+    """
+    p = subprocess.Popen( cmd_clustat ,stdout=subprocess.PIPE ).stdout
+    return p.readlines( )
 
 def find_cluster_members( clustat_output ):
-	"""
-	finds the list of cluster members
+    """
+    finds the list of cluster members
 
-	this function moves through the list clustat_output that is the output
-	of running the clustat binary in RHEL cluster, and snags cluster
-	members based on their Online status.  It finds the node the 
-	clustat command is running on by the Local keyword in the output.
+    this function moves through the list clustat_output that is the output
+    of running the clustat binary in RHEL cluster, and snags cluster
+    members based on their Online status.  It finds the node the 
+    clustat command is running on by the Local keyword in the output.
 
-	the return value is the dictionary containing the node list. 
+    the return value is the dictionary containing the node list. 
 
-	supporting more than 2 node clusters wouldn't be hard, but I'm being lazy.
+    supporting more than 2 node clusters wouldn't be hard, but I'm being lazy.
 
-	"""
-	# nodelist: dict providing local and sibling node entries
-	nodelist = {}
-
-	for line in clustat_output:
-		match = re.search( '^ ([A-Za-z0-9_-]+).*Online, (Local)?.*', line )
-		if match is not None:
-			if match.group(2) is not None:
-				nodelist['local'] = match.group(1) 
-			else:
-				nodelist['node']= match.group(1) 
-
-	reuturn nodelist
+    """
+    # nodelist: dict providing local and sibling node entries
+    nodelist = {}
+    for line in clustat_output:
+        match = re.search( '^ ([A-Za-z0-9_-]+).*Online, (Local)?.*', line )
+        if match is not None:
+            if match.group(2) is not None:
+                nodelist['local'] = match.group(1) 
+            else:
+                nodelist['node']= match.group(1) 
+    return nodelist
 
 def is_service_running(service, thishost, clustat_output ):
-	"""
-	finds if service is running based on clustat_output list 
-	"""
-	for line in clustat_output:
-		match = re.search( '^ service:(%s)[ ]+(%s)[ ]+(started).*' % ( svc, host ) , line )
-		if match is not None:
-			return True
-		else:
-			return False
-
+    """
+    finds if service is running based on clustat_output list
+    """
+    for line in clustat_output:
+        match = re.search( '^ service:(%s)[ ]+(%s)[ ]+(started).*' % ( service, thishost ) , line )
+        if match is not None:
+            # MATCH! Lets return True.
+            return True
+    # We didn't find it.  Return False!
+    return False
 
 def main():
-	usage = "usage: %prog [options]"
-	parser = optparse.OptionParser(usage)
+    usage = "usage: %prog [options]"
+    parser = optparse.OptionParser(usage)
     parser.add_option("-s", "--service", action="store",
-                      help="service name",)
+                      help="service name")
     opts, ____ = parser.parse_args()
 
     # run clustat, get output
@@ -88,9 +88,12 @@ def main():
 
     # if the service is running, on the local node, pass the output and script will:
     if is_service_running( opts.service, cluster_members['local'], output ):
-    	#   exit 0 if the node is running the service
-    	sys.exit(0)
+        #   exit 0 if the node is running the service
+        sys.exit(0)
     else:
-    	#   exit 1 if the node is NOT running the service
-    	sys.exit(1)
+        #   exit 1 if the node is NOT running the service
+        sys.exit(1)
 
+
+if __name__ == '__main__':
+    main()
